bc91ec64baac921b9092a26a0824aade36215526
grelmu <gmurray0505@gmail.com>
2024-09-11 22:42:54 -0400

    Added Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@ function App(): React.JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Grace
+                automatically reload. Grace 
+                Hello World
             </p>
         </div>
     );
